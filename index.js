5b609d56710144c59396f58c363cbed2cec44481
prisprisca <prisca20bamwireku@gmail.com>
2021-06-20 23:37:55 +0300

    correct name for parcels

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
-app.use('/pracels', postRoutes);
+app.use('/parcels', postRoutes);
 
 app.get('/', (req, res) =>{
    res.send('Hello Welcome to On-Time-Delivery-API');
