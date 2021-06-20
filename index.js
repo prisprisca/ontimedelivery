0e3417ee469ee609a8fe6b6436b022b7914fb3d9
prisprisca <prisca20bamwireku@gmail.com>
2021-06-20 22:35:35 +0300

    add pacels route

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use('/pacels', postRoutes);
 
 app.get('/', (req, res) =>{
    res.send('hello');
