e74df7fe963449b55eaf18caa19615612a3774ae
prisprisca <prisca20bamwireku@gmail.com>
2021-06-21 14:36:11 +0300

    add user auth

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,15 @@
 import express from 'express';
 
 import { getOrders, makeOrder, updateOrder, deleteOrder, likeOrder} from'../controllers/posts.js';
+// import auth from '../middleware/auth.js';
 
 const router =express.Router();
 
 router.get('/',getOrders ); 
 router.post('/', makeOrder ); 
-router.patch('/:id', updateOrder);
-router.delete('/:id', deleteOrder);
-router.patch('/:id/likeOrder', likeOrder);
+router.patch('/:id',  updateOrder);
+router.delete('/:id',  deleteOrder);
+router.patch('/:id/likeOrder',  likeOrder);
 
 export default router
 
